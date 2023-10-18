32d4ac051d567c88a60b3173d425e9ac1c82dafb
chanduV7 <chandravarre818@gmail.com>
2023-10-18 11:08:06 +0530

    first commit

---
+++ mongoConfig.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(
+  `mongodb+srv://chandu_V:${process.env.MONGO_PASS}@cluster0.ueuz9jd.mongodb.net/codezo`,
+ { useUnifiedTopology: true },
+  { useNewUrlParser: true },
+  { connectTimeoutMS: 30000 },
+  { keepAlive: 1 }
+);
+
+const db = client.db();
+
+const users =  db.collection("users");
+const jobs = db.collection("jobs");
+
+module.exports = {users, jobs}
